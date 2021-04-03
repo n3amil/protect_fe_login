b709e3afd60c529979f1c170dfb8ce038f268f93
Cypelt <38440086+n3amil@users.noreply.github.com>
2021-04-03 14:41:28 +0000

    Update ext_emconf.php
    version to 0.0.3

--- ext_emconf.php
+++ ext_emconf.php
@@ -11,7 +11,7 @@ $EM_CONF[$_EXTKEY] = array(
     'createDirs'                    => '',
     'modify_tables'                 => '',
     'clearCacheOnLoad'              => 0,
-    'version'                       => '0.0.2',
+    'version'                       => '0.0.3',
     'constraints'                   =>
         array(
             'depends'   =>
